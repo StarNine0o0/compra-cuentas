47a0cd12d26c458e3985b6d11293dc1d94ea5b72
StarNine0o0 <rafaroyale35wow@gmial.com>
2025-06-10 21:14:23 -0600

    primer commit

---
+++ Main.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Date;
+public class Main{
+
+public static void main(String[] args) {
+
+      Scanner sc = new Scanner(System.in);
+      int opc;
+       ArrayList<Compra> compras = new ArrayList<>();
+
+ System.out.print("Ingresa el numero de la cuenta: ");//rellenar datos
+        String numero = sc.nextLine();
+
+        System.out.print("Ingresa el tipo de cuenta (Ej: Debito, Ahorro): ");
+        String tipo = sc.nextLine();
+
+        System.out.print("Ingresa el saldo inicial: ");
+        double saldo = sc.nextDouble();
+
+        Cuenta cuenta = new Cuenta(numero, tipo, saldo);
+
+
+
+
+do{//rellenar compras
+
+      System.out.println( " Descripcion de la compra:");
+      String descripcion =sc.nextLine();
+      sc.nextLine();
+      System.out.println("ingresa el costo de la compra");
+      double costo = sc.nextDouble();
+      sc.nextLine();
+ Compra compra = new Compra(new Date(), descripcion, costo);//objeto tipo compra
+ 
+ cuenta.agregarCompra(compra);//validar con el metodo de compra y se guardan aquii
+
+      System.out.println("agregar otra compra 1(si),2(no)");
+       opc = sc.nextInt();
+}while(opc != 2);
+
+
+
+
+
+
+System.out.println("\nResumen de compras:");
+        for (Compra c : cuenta.getCompras()) {//las compras se guardan en cuenta que es por donde pasan para validarse
+            System.out.println(c.getResumen());
+        }
+
+sc.close();
+
+}
+}
